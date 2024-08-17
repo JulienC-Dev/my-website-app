a43af6674c4776c0b0015de7f652508a607a320a
JulienC-Dev <julien.cormier@yahoo.fr>
2024-08-17 22:13:47 +0200

    wip landing

--- app/(landing)/_terminal/command.ts
+++ app/(landing)/_terminal/command.ts
@@ -3,13 +3,14 @@ export type Command = {
 };
 export const listOfCommand: Command[] = [
   {
-    whoami: `Hey, I'm Julien! I'm a developer who builds websites. After graduating, I worked professionally, focusing on building scalable web applications.`,
+    whoami: `Hey, I'm Julien ! I'm a developer who builds websites. After graduating, I worked professionally, focusing on building scalable web applications.`,
   },
   {
-    help: "Supported commands: ['whoami', 'stack', 'github', 'linkedin', 'domain']",
+    help: "Supported commands: ['whoami', 'stack', 'github', 'linkedin', 'domain', 'art']",
   },
   { github: "https://github.com/JulienC-Dev" },
   { stack: "Python, Next.js, TypeScript, Docker, Kubernetes, AWS, Vercel" },
   { linkedin: "https://www.linkedin.com/in/julien-cormier-41696778/" },
   { domain: "Finance, Cybersecurity, Computer Science" },
+  { art: "Is code a form of art?" },
 ];
